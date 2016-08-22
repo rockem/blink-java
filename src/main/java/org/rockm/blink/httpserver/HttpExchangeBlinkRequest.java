74fbdfea5a7a33766bca307c7f4b60eaaebcb505
Eli Segal <esegal@Administrators-MacBook-Pro.local>
2016-08-22 15:52:54 +0300

    Changed input stream reading implementation

--- src/main/java/org/rockm/blink/httpserver/HttpExchangeBlinkRequest.java
+++ src/main/java/org/rockm/blink/httpserver/HttpExchangeBlinkRequest.java
@@ -3,12 +3,16 @@ package org.rockm.blink.httpserver;
 import com.sun.net.httpserver.HttpExchange;
 import org.rockm.blink.BlinkRequest;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
 
 public class HttpExchangeBlinkRequest implements BlinkRequest {
 
+    private static final int BUFFER_SIZE = 16384;
+    
     private final HttpExchange httpExchange;
     private final String body;
 
@@ -17,7 +21,25 @@ public class HttpExchangeBlinkRequest implements BlinkRequest {
         body = getAsStringFrom(httpExchange.getRequestBody());
     }
 
-    private String getAsStringFrom(InputStream is) {
+    private String getAsStringFrom(InputStream requestBody) {
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+
+        int nRead;
+        byte[] data = new byte[BUFFER_SIZE];
+
+        try {
+            while ((nRead = requestBody.read(data, 0, data.length)) != -1) {
+                buffer.write(data, 0, nRead);
+            }
+            buffer.flush();
+            return buffer.toString("UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /*private String getAsStringFrom(InputStream is) {
         byte[] targetArray = new byte[0];
         try {
             targetArray = new byte[is.available()];
@@ -26,7 +48,7 @@ public class HttpExchangeBlinkRequest implements BlinkRequest {
             // Ignore
         }
         return new String(targetArray);
-    }
+    }*/
 
     @Override
     public String body() {
